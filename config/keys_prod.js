9209882b161efa276f099c2a0cb5606783b42c4f
mariapesm <youandmecanyousee@hotmail.com>
2019-01-10 18:32:20 +0200

    Create keys_prod.js

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  sessionSecret: process.env.SESSION_SECRET
+}

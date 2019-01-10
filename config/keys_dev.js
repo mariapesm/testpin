1f31c4008643cd1e899bf77113a16f0b0974fab9
mariapesm <youandmecanyousee@hotmail.com>
2019-01-10 18:31:28 +0200

    Create keys_dev.js

---
+++ config/keys_dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI:'mongodb://localhost/pinterestclone',
+  googleClientID:'193233367705-5429l689hb3uhbtgnofksda692s6p06m.apps.googleusercontent.com',
+  googleClientSecret:'KmGtLjNGgRgZE_75EHnbhkSa',
+  twitterConsumerKey: 'dhmxn9TAYRk7Yqmhzj6VEnAfV',
+  twitterConsumerSecret: '6tcQ4jtlU6r3IRgb88SAps2NTB1dNp9qpRtfKHFHXAke8BQIOW',
+  sessionSecret: 'TheSecRetK3y'
+}
